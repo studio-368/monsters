42efb7917b51e66699bb1039268984672d773cb6
Paul Gestwicki <pvgestwicki@bsu.edu>
2016-02-11 10:49:09 -0500

    Add AspectRatioTool and HTML full-screening

--- core/src/main/java/edu/bsu/storygame/core/MonsterGame.java
+++ core/src/main/java/edu/bsu/storygame/core/MonsterGame.java
@@ -2,22 +2,33 @@ package edu.bsu.storygame.core;
 
 import edu.bsu.storygame.core.assets.ImageCache;
 import edu.bsu.storygame.core.assets.TileCache;
+import edu.bsu.storygame.core.util.AspectRatioTool;
+import edu.bsu.storygame.core.util.GameBounds;
 import edu.bsu.storygame.core.view.SampleGameScreen;
 import playn.core.Platform;
 import playn.scene.SceneGame;
+import pythagoras.f.IRectangle;
 import tripleplay.game.ScreenStack;
 
 public class MonsterGame extends SceneGame {
+
+    private static final float IPHONE5_VERTICAL_ASPECT_RATIO = 9f / 16f;
 
     private static final int UPDATE_RATE_MS = 33; // 30 times per second
 
     public final ImageCache imageCache;
     public final TileCache tileCache;
+    public final GameBounds bounds;
 
     public MonsterGame(Platform plat) {
         super(plat, UPDATE_RATE_MS);
         imageCache = new ImageCache(plat.assets());
         tileCache = new TileCache(plat.assets());
+
+        IRectangle aspectRatioBox = new AspectRatioTool(IPHONE5_VERTICAL_ASPECT_RATIO)
+                .createBoundingBoxWithin(plat.graphics().viewSize);
+        this.bounds = new GameBounds(aspectRatioBox);
+
         ScreenStack screenStack = new ScreenStack(this, rootLayer);
         screenStack.push(new LoadingScreen(this, screenStack));
     }
---
+++ core/src/main/java/edu/bsu/storygame/core/util/AspectRatioTool.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2015 Paul Gestwicki
+ *
+ * This file is part of The Social Startup Game
+ *
+ * The Social Startup Game is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Social Startup Game is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with The Social Startup Game.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package edu.bsu.storygame.core.util;
+
+import pythagoras.f.IDimension;
+import pythagoras.f.Rectangle;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+public final class AspectRatioTool {
+
+    private final float desiredAspectRatio;
+
+    public AspectRatioTool(float desiredAspectRatio) {
+        checkArgument(desiredAspectRatio > 0, "Aspect ratio must be positive");
+        this.desiredAspectRatio = desiredAspectRatio;
+    }
+
+    // While the local variables are technically unnecessary below, they enhance readability by introducing
+    // a poetic parallel structure. Suppressing the warning.
+    @SuppressWarnings("UnnecessaryLocalVariable")
+    public Rectangle createBoundingBoxWithin(IDimension parent) {
+        final float parentWidth = parent.width();
+        final float parentHeight = parent.height();
+        final float viewAspectRatio = parentWidth / parentHeight;
+        if (desiredAspectRatio < viewAspectRatio) {
+            final float childHeight = parentHeight;
+            final float childWidth = desiredAspectRatio * childHeight;
+            final float xDiff = parentWidth - childWidth;
+            return new Rectangle(xDiff / 2, 0, childWidth, childHeight);
+        } else {
+            final float childWidth = parentWidth;
+            final float childHeight = parentWidth / desiredAspectRatio;
+            final float yDiff = parentHeight - childHeight;
+            return new Rectangle(0, yDiff / 2, childWidth, childHeight);
+        }
+    }
+
+}
