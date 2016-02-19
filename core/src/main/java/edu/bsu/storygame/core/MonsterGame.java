da91b432d0b3dd4b9195f4939a9808065e8a6872
Jack <jtmcginnis@bsu.edu>
2016-02-19 09:22:02 -0700

    Added a skeletal introductory screen to the game. Currently Awaiting proper assets to make the screen look better

--- core/src/main/java/edu/bsu/storygame/core/MonsterGame.java
+++ core/src/main/java/edu/bsu/storygame/core/MonsterGame.java
@@ -5,10 +5,14 @@ import edu.bsu.storygame.core.assets.TileCache;
 import edu.bsu.storygame.core.util.AspectRatioTool;
 import edu.bsu.storygame.core.util.GameBounds;
 import edu.bsu.storygame.core.view.SampleGameScreen;
+import playn.core.Font;
 import playn.core.Platform;
+import playn.core.Pointer;
 import playn.scene.SceneGame;
 import pythagoras.f.IRectangle;
 import tripleplay.game.ScreenStack;
+import tripleplay.ui.Style;
+import tripleplay.ui.Styles;
 
 public class MonsterGame extends SceneGame {
 
@@ -19,17 +23,26 @@ public class MonsterGame extends SceneGame {
     public final ImageCache imageCache;
     public final TileCache tileCache;
     public final GameBounds bounds;
+    public final ScreenStack screenStack;
+    public Styles bigLabel;
+    public Styles bigButton;
 
     public MonsterGame(Platform plat) {
         super(plat, UPDATE_RATE_MS);
         imageCache = new ImageCache(plat.assets());
         tileCache = new TileCache(plat.assets());
+        this.bigLabel = Styles.make(
+                Style.FONT.is(new Font("Times New Roman", 108)),
+                Style.HALIGN.center);
+        this.bigButton = Styles.make(
+                Style.FONT.is(new Font("Times New Roman", 50)),
+                Style.HALIGN.center);
 
         IRectangle aspectRatioBox = new AspectRatioTool(ASPECT_RATIO)
                 .createBoundingBoxWithin(plat.graphics().viewSize);
         this.bounds = new GameBounds(aspectRatioBox);
 
-        ScreenStack screenStack = new ScreenStack(this, rootLayer);
+        screenStack = new ScreenStack(this, rootLayer);
         screenStack.push(new LoadingScreen(this, screenStack));
     }
 }
