b5489efe862ae47942d0f77dba2f9021d0661f67
Brennan Wade <brenw2@Brennans-MacBook-Pro.local>
2016-02-20 17:44:41 -0500

    Merge branch 'master' of /Users/brenw2/Documents/Projects/monsters with conflicts.

--- core/src/main/java/edu/bsu/storygame/core/view/MapView.java
+++ core/src/main/java/edu/bsu/storygame/core/view/MapView.java
@@ -1,12 +1,13 @@
 package edu.bsu.storygame.core.view;
 
-import edu.bsu.storygame.core.assets.ImageCache;
 import edu.bsu.storygame.core.model.GameContext;
 import edu.bsu.storygame.core.model.Phase;
-import pythagoras.f.Dimension;
 import pythagoras.f.IDimension;
-import react.*;
-import tripleplay.ui.*;
+import react.SignalView;
+import react.Slot;
+import tripleplay.ui.Button;
+import tripleplay.ui.Constraints;
+import tripleplay.ui.Group;
 import tripleplay.ui.layout.AxisLayout;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -14,24 +15,24 @@ import static com.google.common.base.Preconditions.checkNotNull;
 public class MapView extends Group {
 
     private final GameContext context;
-    private final IDimension size;
 
     public MapView(GameContext gameContext, IDimension size) {
         super(AxisLayout.horizontal());
         this.context = checkNotNull(gameContext);
-        this.size = new Dimension(size);
-        add(new RegionButton(ImageCache.Key.MAP_LEFT),
-                new RegionButton(ImageCache.Key.MAP_RIGHT));
+        setConstraint(Constraints.fixedSize(size.width(), size.height()));
+        add(new RegionButton(),
+                new RegionButton());
     }
 
     private final class RegionButton extends Button {
-        private static final float PERCENT_OF_HEIGHT = 0.8f;
+        private static final float PERCENT_OF_HEIGHT = 0.4f;
+        private static final float PERCENT_OF_WIDTH = 0.2f;
 
-        RegionButton(ImageCache.Key key) {
-            Icon rawIcon = Icons.image(context.game.imageCache.image(key));
-            float scale = (size.height() * PERCENT_OF_HEIGHT) / rawIcon.height();
-            Icon scaledIcon = Icons.scaled(rawIcon, scale);
-            this.icon.update(scaledIcon);
+        RegionButton() {
+            super();
+            setConstraint(Constraints.fixedSize(
+                    context.game.bounds.width() * PERCENT_OF_WIDTH,
+                    context.game.bounds.height() * PERCENT_OF_HEIGHT));
             context.phase.connect(new Slot<Phase>() {
                 @Override
                 public void onEmit(Phase phase) {
