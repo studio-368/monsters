55ff5bd02da5b7e3e65fe148b7801adc3006541d
Paul Gestwicki <pvgestwicki@bsu.edu>
2016-02-23 13:34:02 -0500

    Refactor for readability

--- core/src/test/java/edu/bsu/storygame/core/view/BiSelectorTest.java
+++ core/src/test/java/edu/bsu/storygame/core/view/BiSelectorTest.java
@@ -1,21 +1,25 @@
 package edu.bsu.storygame.core.view;
 
+import com.google.common.collect.Lists;
 import org.junit.Before;
 import org.junit.Test;
 import tripleplay.ui.ToggleButton;
 
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkState;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class BiSelectorTest {
 
     private BiSelector selector;
-    private ToggleButton button;
-    private ToggleButton secondButton;
+    private List<ToggleButton> buttons;
 
     @Before
     public void setUp() {
         this.selector = new BiSelector();
+        this.buttons = Lists.newArrayList();
     }
 
     @Test
@@ -25,66 +29,74 @@ public class BiSelectorTest {
 
     @Test
     public void testSelect_sizeIsOne() {
-        givenATrackedToggleButton();
-        button.selected().update(true);
-        assertEquals(1, selector.selections().size());
+        givenTrackedToggleButtons(1);
+        whenTheFirstButtonIsToggled();
+        thenTheNumberOfSelectionsIs(1);
     }
 
-    private void givenATrackedToggleButton() {
-        button = new ToggleButton();
-        selector.add(button);
+    private void thenTheNumberOfSelectionsIs(int number) {
+        assertEquals(number, selector.selections().size());
     }
 
-    private void givenTwoTrackedToggleButtons() {
-        button = new ToggleButton();
-        selector.add(button);
-        secondButton = new ToggleButton();
-        selector.add(secondButton);
+    private void whenTheFirstButtonIsToggled() {
+        ToggleButton button = buttons.get(0);
+        button.selected().update(!button.selected().get());
+    }
+
+    private void givenTrackedToggleButtons(int number) {
+        for (; number > 0; number--) {
+            ToggleButton button = new ToggleButton();
+            buttons.add(button);
+            selector.add(button);
+        }
     }
 
     @Test
     public void testDeSelect_sizeIsZero() {
-        givenATrackedToggleButton();
-        button.selected().update(true);
-        button.selected().update(false);
-        assertEquals(0, selector.selections().size());
+        givenTrackedToggleButtons(1);
+        givenButtonsAreSelected(1);
+        whenTheFirstButtonIsToggled();
+        thenTheNumberOfSelectionsIs(0);
+    }
+
+    private void givenButtonsAreSelected(int number) {
+        for (int i = 0; i < number; i++) {
+            buttons.get(i).selected().update(true);
+        }
     }
 
     @Test
-    public void testDeSelect_sizeIsOne() {
-        givenTwoTrackedToggleButtons();
-        button.selected().update(true);
-        secondButton.selected().update(true);
-        button.selected().update(false);
-        assertEquals(1, selector.selections().size());
+    public void testDeselectOneAfterHavingTwoSelected_sizeIsOne() {
+        givenTrackedToggleButtons(2);
+        givenButtonsAreSelected(2);
+        whenTheFirstButtonIsToggled();
+        checkState(!buttons.get(0).selected().get());
+        thenTheNumberOfSelectionsIs(1);
     }
 
     @Test
     public void testSelectElements_sizeIsTwo() {
-        givenTwoTrackedToggleButtons();
-        button.selected().update(true);
-        secondButton.selected().update(true);
+        givenTrackedToggleButtons(2);
+        whenButtonsAreSelected(2);
         assertEquals(2, selector.selections().size());
+    }
+
+    private void whenButtonsAreSelected(int number) {
+        givenButtonsAreSelected(number);
     }
 
     @Test(expected = IllegalStateException.class)
     public void testThreeItemsAdded_twoSelected_thirdSelectionThrowsException() {
-        givenTwoTrackedToggleButtons();
-        ToggleButton thirdButton = new ToggleButton();
-        selector.add(thirdButton);
-        button.selected().update(true);
-        secondButton.selected().update(true);
-        thirdButton.selected().update(true);
+        givenTrackedToggleButtons(3);
+        givenButtonsAreSelected(2);
+        buttons.get(2).selected().update(true);
     }
 
     @Test
     public void testDisableButtonsAfterTwoSelections() {
-        givenTwoTrackedToggleButtons();
-        ToggleButton thirdButton = new ToggleButton();
-        selector.add(thirdButton);
-        button.selected().update(true);
-        secondButton.selected().update(true);
-        assertFalse(thirdButton.isEnabled());
+        givenTrackedToggleButtons(3);
+        whenButtonsAreSelected(2);
+        assertFalse(buttons.get(2).isEnabled());
     }
 
 }
