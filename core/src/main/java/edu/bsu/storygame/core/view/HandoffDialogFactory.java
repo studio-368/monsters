54567c991d020a2536e87a5db1f8b5cb32c6df07
Paul Gestwicki <pvgestwicki@bsu.edu>
2016-03-01 12:08:26 -0500

    Style the handoff popup

--- core/src/main/java/edu/bsu/storygame/core/view/HandoffDialogFactory.java
+++ core/src/main/java/edu/bsu/storygame/core/view/HandoffDialogFactory.java
@@ -2,13 +2,11 @@ package edu.bsu.storygame.core.view;
 
 import edu.bsu.storygame.core.model.GameContext;
 import edu.bsu.storygame.core.model.Phase;
-import playn.core.Color;
 import playn.scene.GroupLayer;
 import playn.scene.Layer;
 import react.Slot;
 import tripleplay.ui.*;
 import tripleplay.ui.layout.AxisLayout;
-import tripleplay.util.Colors;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -22,28 +20,65 @@ public class HandoffDialogFactory {
 
     public Layer create(Interface iface) {
         final GroupLayer layer = new GroupLayer(context.game.bounds.width() / 2f, context.game.bounds.height() / 2f);
-        final Label label = new Label();
         layer.setVisible(false);
-        iface.createRoot(AxisLayout.vertical().offStretch(), GameStyle.newSheet(context.game), layer)
+
+        final HandoffDialog dialog = new HandoffDialog();
+        dialog.setConstraint(Constraints.fixedSize(layer.width(), layer.height()));
+
+        iface.createRoot(AxisLayout.vertical(), GameStyle.newSheet(context.game), layer)
                 .setSize(layer.width(), layer.height())
-                .addStyles(Style.BACKGROUND.is(Background.solid(Color.argb(128, 128, 128, 128))))
-                .add(label.addStyles(Style.COLOR.is(Colors.WHITE)),
-                        new Button("OK").onClick(new Slot<Button>() {
-                            @Override
-                            public void onEmit(Button button) {
-                                context.phase.update(Phase.STORY);
-                            }
-                        }));
+                .add(dialog);
         context.phase.connect(new Slot<Phase>() {
             @Override
             public void onEmit(Phase phase) {
                 if (phase.equals(Phase.HANDOFF)) {
                     final String otherPlayerName = context.otherPlayer().name;
-                    label.text.update("Hand the device to " + otherPlayerName);
+                    dialog.label.text.update("Hand the device to " + otherPlayerName);
                 }
                 layer.setVisible(phase.equals(Phase.HANDOFF));
             }
         });
         return layer;
+    }
+
+    final class HandoffDialog extends Group {
+        final Label label = new StyledLabel();
+
+        private HandoffDialog() {
+            super(AxisLayout.vertical().stretchByDefault());
+            add(new Shim(0, 0),
+                    label.setConstraint(AxisLayout.stretched()),
+                    new OkButton().setConstraint(Constraints.fixedWidth(context.game.bounds.width() * 0.20f)),
+                    new Shim(0, 0));
+        }
+
+        @Override
+        protected Class<?> getStyleClass() {
+            return HandoffDialog.class;
+        }
+    }
+
+    static final class StyledLabel extends Label {
+        @Override
+        protected Class<?> getStyleClass() {
+            return PlayerCreationGroup.StyledLabel.class;
+        }
+    }
+
+    final class OkButton extends Button {
+        private OkButton() {
+            super("Okay");
+            onClick(new Slot<Button>() {
+                @Override
+                public void onEmit(Button button) {
+                    context.phase.update(Phase.STORY);
+                }
+            });
+        }
+
+        @Override
+        protected Class<?> getStyleClass() {
+            return OkButton.class;
+        }
     }
 }
