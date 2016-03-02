5dd418ec62f1fe60d11a01335dc1bd00e70591d0
Paul Gestwicki <pvgestwicki@bsu.edu>
2016-03-02 16:11:24 -0500

    Make turn label visibility reactive to context.currentPlayer

--- core/src/main/java/edu/bsu/storygame/core/view/Sidebar.java
+++ core/src/main/java/edu/bsu/storygame/core/view/Sidebar.java
@@ -2,7 +2,6 @@ package edu.bsu.storygame.core.view;
 
 import edu.bsu.storygame.core.assets.ImageCache;
 import edu.bsu.storygame.core.model.GameContext;
-import edu.bsu.storygame.core.model.Phase;
 import edu.bsu.storygame.core.model.Player;
 import edu.bsu.storygame.core.model.Skill;
 import edu.bsu.storygame.core.util.IconScaler;
@@ -34,20 +33,18 @@ public final class Sidebar extends Group {
         private final SkillGroup skillGroup;
         private PointLabel pointLabel;
         private final Player player;
-        private final TurnLabel turnLabel;
 
         PlayerView(int playerNumber) {
             super(AxisLayout.horizontal());
             this.player = context.players.get(playerNumber);
-
-            IconScaler scaler = new IconScaler(context.game);
-            Icon star = scaler.scale(ImageCache.Key.STAR, context.game.bounds.width() * 0.08f);
+            Icon star = new IconScaler(context.game)
+                    .scale(ImageCache.Key.STAR, context.game.bounds.width() * 0.08f);
 
             Group textGroup = new Group(AxisLayout.vertical().offStretch())
                     .add(new NameLabel(player))
                     .add(skillGroup = new SkillGroup(playerNumber == 0 ? Palette.SPROUT : Palette.BLACK_PEARL))
                     .add(new Shim(1f, 50f))
-                    .add(turnLabel = new TurnLabel(star))
+                    .add(new TurnLabel(star))
                     .setConstraint(AxisLayout.stretched());
 
             pointLabel = new PointLabel(player.storyPoints.get());
@@ -61,8 +58,6 @@ public final class Sidebar extends Group {
 
             watchForSkillChanges();
             watchForPointChange();
-            setTurnVisibility();
-            watchForTurnChange();
             skillGroup.updatePlayerSkills(player);
         }
 
@@ -80,14 +75,6 @@ public final class Sidebar extends Group {
             });
         }
 
-        private void setTurnVisibility() {
-            if (player.equals(context.players.get(0))) {
-                turnLabel.setVisible(true);
-            } else {
-                turnLabel.setVisible(false);
-            }
-        }
-
         private void watchForPointChange() {
             player.storyPoints.connect(new SignalView.Listener<Integer>() {
                 @Override
@@ -97,20 +84,24 @@ public final class Sidebar extends Group {
             });
         }
 
-        private void watchForTurnChange() {
-            context.phase.connect(new Slot<Phase>() {
-                @Override
-                public void onEmit(Phase phase) {
-                    if (phase.equals(Phase.MOVEMENT)) {
-                        changePlayerTurn();
+        final class TurnLabel extends Label {
+            private TurnLabel(Icon icon) {
+                super(icon);
+                context.currentPlayer.connect(new Slot<Player>() {
+                    @Override
+                    public void onEmit(Player player) {
+                        setVisible(player == PlayerView.this.player);
                     }
-                }
-            });
+                });
+                setVisible(context.currentPlayer.get() == PlayerView.this.player);
+            }
+
+            @Override
+            protected Class<?> getStyleClass() {
+                return TurnLabel.class;
+            }
         }
 
-        private void changePlayerTurn() {
-            turnLabel.updateVisibility();
-        }
     }
 
 
@@ -167,27 +158,4 @@ public final class Sidebar extends Group {
             return PointLabel.class;
         }
     }
-
-    final class TurnLabel extends Label {
-        private TurnLabel(Icon icon) {
-            super(icon);
-        }
-
-        private void updateVisibility() {
-            if (this.isVisible()) {
-                this.setVisible(false);
-            } else {
-                this.setVisible(true);
-            }
-        }
-
-        @Override
-        protected Class<?> getStyleClass() {
-            return TurnLabel.class;
-        }
-
-    }
-
-
-
 }
