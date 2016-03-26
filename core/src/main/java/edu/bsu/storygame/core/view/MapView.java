413781c98326209ced0f617d1f4505966af3cd9a
Paul Gestwicki <pvgestwicki@bsu.edu>
2016-03-26 16:07:12 -0400

    Fix point scaling
    The ScalablePicker was no longer needed since Layers.transform automatically transforms points into their _unscaled_ locations. So, clicking on the bottom-right corner of the map always gives 1199,698 (where the image size is 1200,699)--no matter what scale the image is shown on the screen.

--- core/src/main/java/edu/bsu/storygame/core/view/MapView.java
+++ core/src/main/java/edu/bsu/storygame/core/view/MapView.java
@@ -1,57 +1,55 @@
 package edu.bsu.storygame.core.view;
 
+import com.google.common.collect.ImmutableMap;
 import edu.bsu.storygame.core.assets.ImageCache;
 import edu.bsu.storygame.core.model.GameContext;
 import edu.bsu.storygame.core.model.Region;
-import edu.bsu.storygame.core.util.ScalablePicker;
 import playn.core.Pointer;
 import playn.scene.ImageLayer;
-import playn.scene.Layer;
+import pythagoras.f.Point;
 import pythagoras.f.Rectangle;
 import react.Signal;
 import react.SignalView;
 import react.Slot;
+import tripleplay.util.Layers;
+
+import java.util.Map;
 
 public final class MapView extends ImageLayer {
 
-    public final SignalView<Region> pick = Signal.create();
+    /**
+     * The mapping of rectangular regions in the unscaled source image to their regions.
+     */
+    private static final Map<Rectangle, Region> NATURAL_COORDINATE_MAP = ImmutableMap.of(
+            new Rectangle(496, 226, 186, 290), Region.AFRICA,
+            new Rectangle(682, 69, 299, 232), Region.ASIA,
+            new Rectangle(906, 370, 220, 194), Region.OCEANIA,
+            new Rectangle(513, 91, 123, 101), Region.EUROPE,
+            new Rectangle(117, 78, 266, 528), Region.AMERICAS);
 
+    public final SignalView<Region> pick = Signal.create();
     private final GameContext context;
-    private final ScalablePicker<Region> picker = new ScalablePicker<>();
 
     public MapView(GameContext context) {
         super(context.game.imageCache.image(ImageCache.Key.BACKGROUND));
         this.context = context;
         configureMapImagePointer();
-        registerPickableRectangles();
     }
 
     private void configureMapImagePointer() {
         context.game.pointer.events.connect(new Slot<Pointer.Event>() {
+            private final Point local = new Point();
             @Override
             public void onEmit(Pointer.Event event) {
                 if (event.kind.isEnd) {
-                    Region picked = picker.pick(event.x, event.y);
-                    if (picked != null) {
-                        ((Signal<Region>) pick).emit(picked);
+                    Layers.transform(new Point(event.x, event.y), context.game.rootLayer, MapView.this, local);
+                    for (Rectangle r : NATURAL_COORDINATE_MAP.keySet()) {
+                        if (r.contains(local.x, local.y)) {
+                            ((Signal<Region>) pick).emit(NATURAL_COORDINATE_MAP.get(r));
+                        }
                     }
                 }
             }
         });
-    }
-
-    private void registerPickableRectangles() {
-        picker.register(new Rectangle(496, 226, 186, 290), Region.AFRICA);
-        picker.register(new Rectangle(682, 69, 299, 232), Region.ASIA);
-        picker.register(new Rectangle(906, 370, 220, 194), Region.OCEANIA);
-        picker.register(new Rectangle(513, 91, 123, 101), Region.EUROPE);
-        picker.register(new Rectangle(117, 78, 266, 528), Region.AMERICAS);
-    }
-
-    @Override
-    public Layer setScale(float scale) {
-        super.setScale(scale);
-        picker.scale(scale);
-        return this;
     }
 }
