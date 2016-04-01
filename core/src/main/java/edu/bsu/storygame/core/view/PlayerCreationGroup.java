e680c1469e90c86f5550e7689ca9251031eded62
Jessica Lohse <jessicaalohse@gmail.com>
2016-04-01 10:28:33 -0400

    Create region buttons

--- core/src/main/java/edu/bsu/storygame/core/view/PlayerCreationGroup.java
+++ core/src/main/java/edu/bsu/storygame/core/view/PlayerCreationGroup.java
@@ -2,8 +2,7 @@ package edu.bsu.storygame.core.view;
 
 import com.google.common.collect.Lists;
 import edu.bsu.storygame.core.MonsterGame;
-import edu.bsu.storygame.core.model.GameContext;
-import edu.bsu.storygame.core.model.Player;
+import edu.bsu.storygame.core.model.Region;
 import edu.bsu.storygame.core.model.Skill;
 import react.RList;
 import react.Slot;
@@ -25,18 +24,27 @@ public final class PlayerCreationGroup extends Group {
     public final ValueView<Boolean> complete = Value.create(false);
 
     private final MonsterGame game;
-    private final BiSelector selector = new BiSelector();
+    private final BiSelector biselector = new BiSelector();
+    private final Selector selector = new Selector();
 
     public PlayerCreationGroup(MonsterGame game) {
         super(AxisLayout.vertical().offStretch());
         this.game = checkNotNull(game);
+        add(new StyledLabel("Choose two skills:").addStyles(Style.HALIGN.left),
+                createRegionGroup());
         add(new StyledLabel("Choose two skills:").addStyles(Style.HALIGN.left),
                 createSkillGroup());
         watchForFormCompletion();
     }
 
     private void watchForFormCompletion() {
-        selector.selections.connect(new RList.Listener<ToggleButton>() {
+        selector.selected.connect(new Slot<Element<?>>() {
+            @Override
+            public void onEmit(Element<?> element) {
+                checkForCompletion();
+            }
+        });
+        biselector.selections.connect(new RList.Listener<ToggleButton>() {
             @Override
             public void onAdd(ToggleButton elem) {
                 checkForCompletion();
@@ -50,7 +58,8 @@ public final class PlayerCreationGroup extends Group {
     }
 
     private void checkForCompletion() {
-        final boolean isComplete = selector.size() == NUMBER_OF_SKILLS;
+        final boolean isComplete = biselector.size() == NUMBER_OF_SKILLS
+                && ((RegionButton)selector.selected.get()).region != null;
         ((Value<Boolean>) complete).update(isComplete);
     }
 
@@ -59,15 +68,25 @@ public final class PlayerCreationGroup extends Group {
         Group group = new Group(new FlowLayout());
         for (Skill skill : skillSet) {
             SkillButton skillButton = new SkillButton(skill);
-            selector.add(skillButton);
+            biselector.add(skillButton);
             group.add(skillButton);
+        }
+        return group;
+    }
+
+    private Group createRegionGroup(){
+        Group group = new Group(new FlowLayout());
+        for(Region region : Region.values()){
+            RegionButton regionButton = new RegionButton(region);
+            selector.add(regionButton);
+            group.add(regionButton);
         }
         return group;
     }
 
     public List<Skill> getSelectedSkills() {
         List<Skill> result = Lists.newArrayList();
-        for (ToggleButton button : selector.selections()) {
+        for (ToggleButton button : biselector.selections()) {
             Skill skill = ((SkillButton) button).skill;
             result.add(skill);
         }
@@ -84,6 +103,24 @@ public final class PlayerCreationGroup extends Group {
         SkillButton(Skill skill) {
             super(skill.name);
             this.skill = skill;
+            setConstraint(Constraints.fixedWidth(
+                    game.bounds.width() * PERCENT_OF_WIDTH));
+        }
+
+        @Override
+        protected Class<?> getStyleClass() {
+            return SkillButton.class;
+        }
+    }
+
+    final class RegionButton extends ToggleButton {
+        private static final float PERCENT_OF_WIDTH = 0.22f;
+
+        final Region region;
+
+        RegionButton(Region region) {
+            super(region.name().toLowerCase().replace("_", " "));
+            this.region = region;
             setConstraint(Constraints.fixedWidth(
                     game.bounds.width() * PERCENT_OF_WIDTH));
         }
