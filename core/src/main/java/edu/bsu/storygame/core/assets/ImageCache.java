5d9b8523b0bddf13e70082247b1fd99da7adca86
Brenw2 <Brenw2@gmail.com>
2016-04-06 19:19:26 -0400

    Add first and second intro image

--- core/src/main/java/edu/bsu/storygame/core/assets/ImageCache.java
+++ core/src/main/java/edu/bsu/storygame/core/assets/ImageCache.java
@@ -37,7 +37,9 @@ public class ImageCache {
         TANIWHA("taniwha.png"),
         ADZE("adze.png"),
         YOWIE("yowie.png"),
-        WENDIGO("wendigo.png");
+        WENDIGO("wendigo.png"),
+        INTRO_SCENE_2("intro_scene_2.png"),
+        INTRO_SCENE_1("intro_scene_1.png");
 
         private final String path;
 
