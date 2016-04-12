198d90c7013d4bc4c36ad23b63821bf982dcbc93
Paul Gestwicki <pvgestwicki@bsu.edu>
2016-04-12 16:06:58 -0400

    Update copyright and licensing information

--- core/src/main/java/edu/bsu/storygame/core/util/GameBounds.java
+++ core/src/main/java/edu/bsu/storygame/core/util/GameBounds.java
@@ -1,20 +1,20 @@
 /*
- * Copyright 2015 Paul Gestwicki
+ * Copyright 2016 Paul Gestwicki
  *
- * This file is part of The Social Startup Game
+ * This file is part of Traveler's Notebook: Monster Tales
  *
- * The Social Startup Game is free software: you can redistribute it and/or modify
+ * Traveler's Notebook: Monster Tales is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * The Social Startup Game is distributed in the hope that it will be useful,
+ * Traveler's Notebook: Monster Tales is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with The Social Startup Game.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Traveler's Notebook: Monster Tales.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package edu.bsu.storygame.core.util;
--- core/src/main/java/edu/bsu/storygame/core/util/IconScaler.java
+++ core/src/main/java/edu/bsu/storygame/core/util/IconScaler.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2016 Paul Gestwicki
+ *
+ * This file is part of Traveler's Notebook: Monster Tales
+ *
+ * Traveler's Notebook: Monster Tales is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Traveler's Notebook: Monster Tales is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Traveler's Notebook: Monster Tales.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package edu.bsu.storygame.core.util;
 
 import edu.bsu.storygame.core.MonsterGame;
@@ -6,7 +25,7 @@ import playn.core.Image;
 import tripleplay.ui.Icon;
 import tripleplay.ui.Icons;
 
-import static com.google.common.base.Preconditions.*;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public final class IconScaler {
 
