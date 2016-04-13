e621449ac63d73535b83283b24d54ae5ec90ceb5
Paul Gestwicki <pvgestwicki@bsu.edu>
2016-04-13 10:18:17 -0400

    Add copyright and license information

--- core/src/main/java/edu/bsu/storygame/core/model/Story.java
+++ core/src/main/java/edu/bsu/storygame/core/model/Story.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2016 Traveler's Notebook: Monster Tales project authors
+ *
+ * This file is part of monsters
+ *
+ * monsters is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * monsters is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with monsters.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package edu.bsu.storygame.core.model;
 
 import com.google.common.base.MoreObjects;
@@ -7,7 +26,7 @@ import com.google.common.collect.Lists;
 import java.util.List;
 import java.util.Objects;
 
-import static com.google.common.base.Preconditions.*;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public final class Story {
 
