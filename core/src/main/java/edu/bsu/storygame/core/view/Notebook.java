42fbc50444323d7c397840bf5dc9c58e1d36ab82
Paul Gestwicki <pvgestwicki@bsu.edu>
2016-04-16 07:47:41 -0400

    Introduce Page abstraction
    This makes it easier to reason about which page is where, since now there are more clear leftPages and rightPages stacks. This matches a reader's mental model for a book (as long as the reader has taken CS2, of course).

--- core/src/main/java/edu/bsu/storygame/core/view/Notebook.java
+++ core/src/main/java/edu/bsu/storygame/core/view/Notebook.java
@@ -19,7 +19,7 @@
 
 package edu.bsu.storygame.core.view;
 
-import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import edu.bsu.storygame.core.MonsterGame;
 import playn.scene.GroupLayer;
 import playn.scene.Layer;
@@ -28,9 +28,14 @@ import pythagoras.f.IRectangle;
 import pythagoras.f.Rectangle;
 import react.RFuture;
 import react.RPromise;
+import react.Slot;
 import tripleplay.anim.Animation;
 import tripleplay.anim.Animator;
 import tripleplay.shaders.RotateYBatch;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Stack;
 
 import static com.google.common.base.Preconditions.*;
 
@@ -41,11 +46,10 @@ public class Notebook extends GroupLayer {
 
     private final MonsterGame game;
     private final Animator anim;
-    private ImmutableList<Layer> layers;
-    private RotateYBatch batch;
     private final IRectangle openBounds;
-    private int rightTopPageIndex = 0;
     private boolean flipping = false;
+    private final Stack<Page> rightPages = new Stack<>();
+    private final Stack<Page> leftPages = new Stack<>();
 
     public Notebook(MonsterGame game, Animator anim, IRectangle openBounds, Layer... layers) {
         super(game.plat.graphics().viewSize.width(), game.plat.graphics().viewSize.height());
@@ -54,73 +58,137 @@ public class Notebook extends GroupLayer {
         this.game = game;
         this.openBounds = new Rectangle(openBounds);
         this.anim = checkNotNull(anim);
-        this.layers = ImmutableList.copyOf(layers);
 
-        for (int i = layers.length - 1; i >= 0; i -= 2) {
-            addAt(layers[i], openBounds.x() + openBounds.width() / 2, openBounds.y());
+        assembleBookFrom(layers);
+    }
+
+    private void assembleBookFrom(Layer[] layers) {
+        List<Page> pages = Lists.newArrayList();
+        for (int i = 0; i < layers.length; i += 2) {
+            Page page = (i == layers.length - 1) ? new Page(layers[i]) : new Page(layers[i], layers[i + 1]);
+            pages.add(page);
         }
+        for (int i = pages.size() - 1; i >= 0; i--) {
+            Page page = pages.get(i);
+            rightPages.push(page);
+            addAtClosedPageLocation(page.front);
+        }
+    }
+
+    private void addAtClosedPageLocation(Layer layer) {
+        addAt(layer, openBounds.x() + openBounds.width() / 2, openBounds.y());
     }
 
     public RFuture<Notebook> turnPage() {
         checkState(!flipping, "Already flipping, cannot deal with it");
+        checkState(rightPages.size() > 1, "Cannot turn last page");
         flipping = true;
-        checkState(rightTopPageIndex < layers.size() - 1, "On last page, can not flip any more.");
 
         final RPromise<Notebook> promise = RPromise.create();
-        final Layer page = layers.get(rightTopPageIndex);
-        final Layer reverse = layers.get(rightTopPageIndex + 1);
-        batch = new RotateYBatch(game.plat.graphics().gl, 0.5f, 0.5f, 1.5f);
-        page.setBatch(batch);
-
-        BatchAngleAnimator batchAngle = new BatchAngleAnimator(batch);
-        anim.tween(batchAngle)
-                .from(0)
-                .to(FloatMath.HALF_PI)
-                .in(FLIP_DURATION / 2)
-                .easeIn()
-                .then()
-                .action(new Runnable() {
-                    @Override
-                    public void run() {
-                        remove(page);
-                        addAt(reverse, openBounds.x() + openBounds.width() / 2, openBounds.y());
-                        reverse.setBatch(batch);
-                    }
-                })
-                .then()
-                .tween(batchAngle)
-                .from(FloatMath.HALF_PI)
-                .to(FloatMath.PI)
-                .in(FLIP_DURATION / 2)
-                .easeOut()
-                .then()
-                .action(new Runnable() {
-                    @Override
-                    public void run() {
-                        rightTopPageIndex += 2;
-                        flipping = false;
-                        promise.succeed(Notebook.this);
-                    }
-                });
+        final Page page = rightPages.pop();
+        page.turnLeft().onSuccess(new Slot<Page>() {
+            @Override
+            public void onEmit(Page page) {
+                promise.succeed(Notebook.this);
+            }
+        });
         return promise;
     }
 
-    public void closeBook() {
+    public RFuture<Notebook> closeBook() {
         checkState(!flipping, "Animation progress, cannot deal with it");
-        checkState(rightTopPageIndex != 0, "Already closed");
+        checkState(!leftPages.isEmpty(), "Already closed");
         flipping = true;
 
+        final RPromise<Notebook> promise = RPromise.create();
+        final List<RFuture<Page>> pageClosings = Lists.newArrayList();
+
         int delay = 0;
-        for (int pageIndex = rightTopPageIndex - 1; pageIndex >= 0; pageIndex -= 2) {
+        while (!leftPages.isEmpty()) {
             final RotateYBatch batch = new RotateYBatch(game.plat.graphics().gl, 0.5f, 0.5f, 1.5f);
+            final Page page = leftPages.pop();
             batch.angle = FloatMath.PI;
-            final Layer page = layers.get(pageIndex);
-            final Layer reverse = layers.get(pageIndex - 1);
-            page.setBatch(batch);
-            BatchAngleAnimator batchAngle = new BatchAngleAnimator(batch);
+            page.back.setBatch(batch);
             anim.delay(delay)
                     .then()
+                    .action(new Runnable() {
+                        @Override
+                        public void run() {
+                            pageClosings.add(page.turnRight());
+                        }
+                    });
+            delay += DELAY_BETWEEN_CLOSING_PAGES;
+        }
+        RFuture.collect(pageClosings).onSuccess(new Slot<Collection<Page>>() {
+            @Override
+            public void onEmit(Collection<Page> pages) {
+                flipping = false;
+                promise.succeed(Notebook.this);
+            }
+        });
+        return promise;
+    }
+
+    private final class Page {
+        final Layer front;
+        final Layer back;
+
+        Page(Layer front) {
+            this.front = checkNotNull(front);
+            this.back = null;
+        }
+
+        Page(Layer front, Layer back) {
+            checkArgument(front != back);
+            this.front = checkNotNull(front);
+            this.back = checkNotNull(back);
+        }
+
+        RFuture<Page> turnLeft() {
+            final RPromise<Page> promise = RPromise.create();
+            final RotateYBatch batch = new RotateYBatch(game.plat.graphics().gl, 0.5f, 0.5f, 1.5f);
+            front.setBatch(batch);
+
+            BatchAngleAnimator batchAngle = new BatchAngleAnimator(batch);
+            anim.tween(batchAngle)
+                    .from(0)
+                    .to(FloatMath.HALF_PI)
+                    .in(FLIP_DURATION / 2)
+                    .easeIn()
+                    .then()
+                    .action(new Runnable() {
+                        @Override
+                        public void run() {
+                            remove(front);
+                            addAtClosedPageLocation(back);
+                            back.setBatch(batch);
+                        }
+                    })
+                    .then()
                     .tween(batchAngle)
+                    .from(FloatMath.HALF_PI)
+                    .to(FloatMath.PI)
+                    .in(FLIP_DURATION / 2)
+                    .easeOut()
+                    .then()
+                    .action(new Runnable() {
+                        @Override
+                        public void run() {
+                            flipping = false;
+                            leftPages.push(Page.this);
+                            promise.succeed(Page.this);
+                        }
+                    });
+            return promise;
+        }
+
+        RFuture<Page> turnRight() {
+            final RPromise<Page> promise = RPromise.create();
+            final RotateYBatch batch = new RotateYBatch(game.plat.graphics().gl, 0.5f, 0.5f, 1.5f);
+            batch.angle = FloatMath.PI;
+            back.setBatch(batch);
+            BatchAngleAnimator batchAngle = new BatchAngleAnimator(batch);
+            anim.tween(batchAngle)
                     .from(FloatMath.PI)
                     .to(FloatMath.HALF_PI)
                     .in(FLIP_DURATION / 2)
@@ -129,9 +197,9 @@ public class Notebook extends GroupLayer {
                     .action(new Runnable() {
                         @Override
                         public void run() {
-                            remove(page);
-                            addAt(reverse, openBounds.x() + openBounds.width() / 2, openBounds.y());
-                            reverse.setBatch(batch);
+                            remove(back);
+                            addAtClosedPageLocation(front);
+                            front.setBatch(batch);
                         }
                     })
                     .then()
@@ -144,13 +212,11 @@ public class Notebook extends GroupLayer {
                     .action(new Runnable() {
                         @Override
                         public void run() {
-                            rightTopPageIndex -= 2;
-                            if (rightTopPageIndex == 0) {
-                                flipping = false;
-                            }
+                            rightPages.push(Page.this);
+                            promise.succeed(Page.this);
                         }
                     });
-            delay += DELAY_BETWEEN_CLOSING_PAGES;
+            return promise;
         }
     }
 
